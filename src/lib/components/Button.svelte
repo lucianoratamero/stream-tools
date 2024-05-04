565ba20cee04d44928659a8be3090836cce2f763
Luciano Ratamero <luciano@ratamero.com>
2024-05-03 23:42:04 -0300

    Add button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	const {
+		children,
+		type = 'button',
+		...rest
+	}: HTMLButtonAttributes & { children: Snippet } = $props();
+</script>
+
+<button
+	{type}
+	{...rest}
+	class="rounded border border-zinc-800 px-4 py-2 shadow-lg transition-all active:scale-95"
+>
+	{@render children()}
+</button>
