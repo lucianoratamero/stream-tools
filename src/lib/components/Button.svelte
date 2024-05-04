0c82d0dd4280e490c7b560c2b7f16987d74afb5e
Luciano Ratamero <luciano@ratamero.com>
2024-05-04 01:08:15 -0300

    Add disabled state to button component

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -5,14 +5,21 @@
 	const {
 		children,
 		type = 'button',
+		disabled,
 		...rest
 	}: HTMLButtonAttributes & { children: Snippet } = $props();
+
+	const disabledClasses = disabled ? 'opacity-50 cursor-not-allowed' : 'active:scale-95';
 </script>
 
 <button
 	{type}
+	{disabled}
 	{...rest}
-	class="rounded border border-zinc-800 px-4 py-2 shadow-lg transition-all active:scale-95"
+	class={`
+    ${disabledClasses}
+    rounded border border-zinc-800 px-4 py-2 shadow-lg transition-all
+  `}
 >
 	{@render children()}
 </button>
