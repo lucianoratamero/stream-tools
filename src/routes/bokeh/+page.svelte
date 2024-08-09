19a743307eeb80541d0c3bbb6a65d3203c4f41a3
Luciano Ratamero <luciano@ratamero.com>
2024-08-09 18:18:31 -0300

    Fix bokeh page form

--- src/routes/bokeh/+page.svelte
+++ src/routes/bokeh/+page.svelte
@@ -8,6 +8,7 @@
 	import colorPalettes from 'nice-color-palettes/1000.json';
 	import { onMount } from 'svelte';
 	import { Icon, Trash } from 'svelte-hero-icons';
+	import { fade } from 'svelte/transition';
 
 	let history = $state<{ colorPalette: string[]; name?: string }[]>([]);
 	let bookmarks = $state<{ colorPalette: string[]; name: string }[]>([]);
@@ -77,63 +78,70 @@
 	});
 </script>
 
-<form
-	class="fixed right-4 top-4 z-10 flex flex-col gap-2 rounded-lg bg-white p-4 shadow-xl"
-	class:hidden={!showForm}
->
-	<p>Click on the canvas to hide the form :]</p>
-	<p>
-		Decay (recommended: between 0.2 and 1.8):
-		<input class="border p-1" bind:value={decay} />
-	</p>
-	<div class="flex gap-2">
-		<Button onclick={changePallette}>Change color palette</Button>
-		<Button onclick={goFullscreen}>Go fullscreen</Button>
-	</div>
-	<div class="flex gap-2">
-		<Button href={`${base}/bokeh/history`}>See history</Button>
-		<Button href={`${base}/bokeh/create`}>Create palette</Button>
-	</div>
+{#if showForm}
+	<form
+		in:fade
+		class="fixed right-4 top-4 z-10 flex flex-col gap-2 rounded-lg bg-white p-4 shadow-xl"
+	>
+		<p>Click on the canvas to hide the form :]</p>
+		<p>
+			Decay (recommended: between 0.2 and 1.8):
+			<input class="border p-1" bind:value={decay} />
+		</p>
+		<div class="flex gap-2">
+			<Button onclick={changePallette}>Change color palette</Button>
+			<Button onclick={goFullscreen}>Go fullscreen</Button>
+		</div>
+		<div class="flex gap-2">
+			<Button href={`${base}/bokeh/history`}>See history</Button>
+			<Button href={`${base}/bokeh/create`}>Create palette</Button>
+		</div>
 
-	<section class="my-2">
-		<h2 class="text-lg">Current palette</h2>
-		<PaletteCard
-			isPlaceholder
-			palette={colorPalette}
-			bind:name={placeholderName}
-			i={colorPalettes.findIndex((p) => JSON.stringify(p) === JSON.stringify(colorPalette))}
-			{colorPalettes}
-			bind:history
-			bind:bookmarks
-			bind:colorPalette
-		/>
-	</section>
+		<section class="my-2">
+			<h2 class="text-lg">Current palette</h2>
+			<PaletteCard
+				isPlaceholder
+				palette={colorPalette}
+				bind:name={placeholderName}
+				i={colorPalettes.findIndex((p) => JSON.stringify(p) === JSON.stringify(colorPalette))}
+				{colorPalettes}
+				bind:history
+				bind:bookmarks
+				bind:colorPalette
+			/>
+		</section>
 
-	<h2 class="text-lg">Bookmarks</h2>
-	<div class="grid grid-cols-3 gap-2">
-		{#each bookmarks as { name }, i}
-			<Button class="flex items-center justify-between gap-2" href={`${base}/?bookmark=${name}`}>
-				{name || `Bookmark ${i + 1}`}
-				<Button
-					type="button"
-					class="border-red-800 bg-red-300 !p-1"
-					title="Remove bookmark"
-					onclick={() => (bookmarks = bookmarks.filter((item) => item.name !== name))}
-				>
-					<Icon src={Trash} class="h-4 w-4" />
-				</Button>
-			</Button>
-		{:else}
-			<div class="text-center col-span-3">
-				<p class="text-gray-500 mb-2">No bookmarks yet</p>
-				<p>
-					<Button href={`${base}/bokeh/create`}>Create one</Button>
-					<span class="px-2">or</span>
-					<Button href={`${base}/bokeh/history`}>See history</Button>
-				</p>
-			</div>
-		{/each}
-	</div>
-</form>
+		<h2 class="text-lg">Bookmarks</h2>
+		<div class="grid grid-cols-3 gap-2">
+			{#each bookmarks as { name }, i}
+				<span class="flex" in:fade>
+					<Button
+						class="flex grow items-center justify-between gap-2 rounded-r-none border-r-0"
+						href={`${base}/bokeh?bookmark=${name}`}
+					>
+						{name || `Bookmark ${i + 1}`}
+					</Button>
+					<Button
+						type="button"
+						class="rounded-l-none border-red-800 bg-red-300"
+						title="Remove bookmark"
+						onclick={() => (bookmarks = bookmarks.filter((item) => item.name !== name))}
+					>
+						<Icon src={Trash} class="h-4 w-4" />
+					</Button>
+				</span>
+			{:else}
+				<div class="text-center col-span-3">
+					<p class="text-gray-500 mb-2">No bookmarks yet</p>
+					<p>
+						<Button href={`${base}/bokeh/create`}>Create one</Button>
+						<span class="px-2">or</span>
+						<Button href={`${base}/bokeh/history`}>See history</Button>
+					</p>
+				</div>
+			{/each}
+		</div>
+	</form>
+{/if}
 
 <BokehPage {colorPalette} {transparentBg} {decay} bind:showForm />
