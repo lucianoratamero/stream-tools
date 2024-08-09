0cffe60390c918fc5c62ff1c4ded0d12bf89730a
Luciano Ratamero <luciano@ratamero.com>
2024-08-09 18:18:44 -0300

    Add confetti and bokeh effects

--- src/routes/effects/+page.svelte
+++ src/routes/effects/+page.svelte
@@ -1,21 +1,60 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import scanlines from '$lib/img/scanlines.png';
 	import { onMount } from 'svelte';
+	import Confetti from 'svelte-confetti';
 
 	let crt_effect_enabled = $state(false);
+	let confetti_effect_enabled = $state(false);
+	let bokeh_effect_enabled = $state(false);
+	let bokeh_options: { transparent_bg?: boolean; bookmark?: string } = { transparent_bg: false };
 
 	onMount(() => {
-		const searchParams = $page.url.searchParams;
+		const search_params = $page.url.searchParams;
 
-		if (searchParams.has('crt')) {
+		if (search_params.has('crt')) {
 			crt_effect_enabled = true;
+		}
+		if (search_params.has('confetti')) {
+			confetti_effect_enabled = true;
+		}
+		if (search_params.has('bokeh')) {
+			bokeh_effect_enabled = true;
+			if (search_params.has('transparentBg')) {
+				bokeh_options.transparent_bg = search_params.get('transparentBg') !== 'false';
+			}
+			if (search_params.has('bookmark')) {
+				bokeh_options.bookmark = search_params.get('bookmark') as string;
+			}
 		}
 	});
 </script>
 
 {#if crt_effect_enabled}
 	<img src={scanlines} class="crt size fixed h-full w-full bg-cover" alt="CRT background effect" />
+{/if}
+
+{#if confetti_effect_enabled}
+	<div class="absolute left-1/2 top-[-1rem] z-10">
+		<Confetti
+			size={20}
+			amount={200}
+			duration={6000}
+			y={[1, 2]}
+			x={[-6, 6]}
+			fallDistance="120vh"
+			infinite
+		/>
+	</div>
+{/if}
+
+{#if bokeh_effect_enabled}
+	<iframe
+		title="bokeh"
+		src={`${base}/bokeh?${bokeh_options.transparent_bg ? '&transparentBg' : ''}${bokeh_options.bookmark ? `&bookmark=${bokeh_options.bookmark}` : ''}`}
+		class="fixed h-full w-full bg-cover"
+	></iframe>
 {/if}
 
 <style>
