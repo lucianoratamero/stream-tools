3fb1a6bad5640a02651c06cc7a87fc8d99e255c3
Luciano Ratamero <luciano@ratamero.com>
2024-08-09 19:46:50 -0300

    Add tailwind typography to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts,md}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('@tailwindcss/typography')]
+};
