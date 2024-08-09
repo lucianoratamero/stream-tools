e66f110da4294b7eef068558ba01dd4a33339171
Luciano Ratamero <luciano@ratamero.com>
2024-08-09 18:17:42 -0300

    Remove body bg color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
 	import { base } from '$app/paths';
 </script>
 
-<div class="flex h-screen flex-col items-center justify-center gap-4 text-white">
-	<h1 class="text-4xl">Tools</h1>
-	<ul class="text-xl">
+<div class="flex h-screen gap-4">
+	<h1 class="text-8xl">Tools</h1>
+	<ul class="text-4xl">
 		<li>
 			<a href={`${base}/bokeh`}>Bokeh</a>
 		</li>
