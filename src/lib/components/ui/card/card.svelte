d5f8e95e2b8b636137e64b627c7f7160b9aee6f1
Luciano Ratamero <luciano@ratamero.com>
2024-08-18 01:02:35 -0300

    Add shadcn-svelte and components

---
+++ src/lib/components/ui/card/card.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { cn } from '$lib/utils.js';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type $$Props = HTMLAttributes<HTMLDivElement>;
+
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+</script>
+
+<div
+	class={cn('bg-card text-card-foreground rounded-lg border shadow-sm', className)}
+	{...$$restProps}
+>
+	<slot />
+</div>
