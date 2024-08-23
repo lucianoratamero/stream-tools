ff1172a2c30367fa63a3509a1211a6a9e509f000
Luciano Ratamero <luciano@ratamero.com>
2024-08-23 14:48:50 -0300

    Add md files to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import { fontFamily } from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 const config = {
 	darkMode: ['class'],
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts,md}'],
 	safelist: ['dark'],
 	theme: {
 		container: {
