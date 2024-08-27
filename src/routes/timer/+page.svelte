a177a9bbba0afbe3939538fa134ff3286911e6d2
Luciano Ratamero <luciano@ratamero.com>
2024-08-26 23:01:29 -0300

    Add more audio effects and youtube video playback to timer page

--- src/routes/timer/+page.svelte
+++ src/routes/timer/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import confetti_audio from '$lib/audio/confetti.mp3';
+	import metal_pipe_falling from '$lib/audio/metal-pipe-falling.mp3';
+	import toc_toc from '$lib/audio/toc-toc.mp3';
 	import mario_party_finish from '$lib/audio/mario-party-finish.mp3';
 	import { onMount } from 'svelte';
 	import { Confetti } from 'svelte-confetti';
@@ -9,6 +11,8 @@
 	let no_audio = $state(false);
 	let show_timer = $state(false);
 	let invisible_timer = $state(false);
+	let audio: 'finish' | 'toctoc' | 'pipe' = $state('finish');
+	let video: string | undefined = $state();
 	let show_error = $state();
 	let formatted_time = $derived.by(() => formatTime(countdown_time));
 
@@ -38,6 +42,15 @@
 
 		if (searchParams.has('invisible')) {
 			invisible_timer = true;
+		}
+
+		if (searchParams.has('audio')) {
+			audio = searchParams.get('audio') as 'finish' | 'toctoc' | 'pipe';
+		}
+
+		if (searchParams.has('video')) {
+			const yt_url = new URL(searchParams.get('video') as string);
+			video = `https://www.youtube.com/embed/${yt_url.searchParams.get('v') as string}?autoplay=1&amp;controls=0`;
 		}
 
 		countdown_time = Number(searchParams.get('timeInSeconds'));
@@ -74,8 +87,25 @@
 					{formatted_time}
 				</p>
 				{#if !no_audio}
-					<audio src={confetti_audio} autoplay></audio>
-					<audio src={mario_party_finish} autoplay></audio>
+					{#if audio === 'pipe'}
+						<audio src={metal_pipe_falling} autoplay></audio>
+					{:else if audio === 'toctoc'}
+						<audio src={toc_toc} autoplay></audio>
+					{:else if !video}
+						<audio src={confetti_audio} autoplay></audio>
+						<audio src={mario_party_finish} autoplay></audio>
+					{/if}
+
+					{#if video}
+						<iframe
+							title="youtube video"
+							class="absolute top-0 left-0 w-full h-full"
+							src={video}
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen
+						></iframe>
+					{/if}
 				{/if}
 				<div class="absolute left-1/2 top-[95%]">
 					<Confetti
