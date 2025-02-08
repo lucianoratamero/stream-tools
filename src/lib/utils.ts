e471c20df8abef78daf583b4ea6ca85c9b50ed81
Luciano Ratamero <luciano@ratamero.com>
2025-02-08 00:14:11 -0300

    Add new utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -60,3 +60,30 @@ export const flyAndScale = (
 		easing: cubicOut
 	};
 };
+
+export const get_random_from_array = <T>(arr: T[]): T => {
+	return arr[Math.floor(Math.random() * arr.length)];
+};
+
+export function range(start: number, stop?: number, step?: number): number[] {
+  if (typeof stop == 'undefined') {
+      // one param defined
+      stop = start;
+      start = 0;
+  }
+
+  if (typeof step == 'undefined') {
+      step = 1;
+  }
+
+  if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+      return [];
+  }
+
+  var result = [];
+  for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+      result.push(i);
+  }
+
+  return result;
+};
